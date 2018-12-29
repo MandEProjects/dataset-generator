e20f9f99f2cb475e6f4fa039044958727422b214
EdwynPeignon <31069016+EdwynPeignon@users.noreply.github.com>
2018-12-29 12:18:05 +0100

    Add files via upload
    App --> generate a bulk json
    config.yaml --> setting app

---
+++ people.py
@@ -0,0 +1,7 @@
+class People:
+
+    def __init__(self):
+        self.lastName = str()
+        self.firstName = str()
+        self.age = int()
+        self.followers = int()
---
+++ tweet.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+class Tweet:
+
+    def __init__(self):
+        self.geo = tuple
+        self.city = str()
+        self.date = datetime()
+        self.text = str()
+        self.like = int()
+        self.lastName = str()
+        self.firstName = str()
+
